97812404589174147689a175bfc8b0b33928df7d
Peter McCullough <peter.mccullough@atkinsglobal.com>
2021-08-30 17:26:12 +0100

    First commit

---
+++ src/tinyfps.js
@@ -0,0 +1,13 @@
+
+/**
+ * @author pete
+ */
+
+var tiny = function() {
+
+    var container = document.createElement('div');
+
+    var beginTime = ( performance || Date ).now(), prevTime = beginTime, frames = 0;
+
+
+}
