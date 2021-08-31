5c49cf6a8a0743f61138ee5bbc32d2b771f9a135
Peter McCullough <peter.mccullough@atkinsglobal.com>
2021-08-31 11:32:33 +0100

    finished up tinyfps

--- src/tinyfps.js
+++ src/tinyfps.js
@@ -3,11 +3,48 @@
  * @author pete
  */
 
-var tiny = function() {
+class tiny {
 
-    var container = document.createElement('div');
+    constructor() {
+        this.container = document.createElement('div');
+        this.container.style.fontSize = "14px";
 
-    var beginTime = ( performance || Date ).now(), prevTime = beginTime, frames = 0;
+        this.lastTickTime = 0;
+        this.lastUpdate = 0;
+    }
 
+    tick(){
+        var delta = ( ( performance || Date ).now() - this.lastTickTime ) / 1000;
 
+        var fps = Math.round( ( 1 / delta ) );
+
+        this.lastTickTime = ( performance || Date).now();
+
+        if( ( this.lastTickTime - this.lastUpdate >= 500 ) ){
+
+            this._update(fps);
+
+            this.lastUpdate = this.lastTickTime;
+        }
+    }
+
+    _update(fps){
+
+        var colour;
+
+        switch(fps){
+            case fps < 20:
+                colour = "red";
+                break;
+            
+            case fps < 50:
+                colour = "amber";
+
+            default:
+                colour = "green";
+        }
+
+        this.container.textContent= `${ fps } FPS`;
+        this.container.style.color = colour;
+    }
 }
